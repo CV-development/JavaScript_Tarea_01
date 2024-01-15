19b17715f8feba4cfada45178a4b3282849a96ae
Carlos Valderas <carlos.valderas.a@gmail.com>
2024-01-14 21:49:48 -0300

    Subida inicial, pedido aplicado

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const precio = 400000
+precioSpan = document.querySelector(".precio-inicial")
+precioSpan.innerHTML = precio
+
+cantidadItems = 0
+cantSpan = document.querySelector(".cantidad")
+cantSpan.innerHTML = cantidadItems
+
+precioTotal = document.querySelector(".valor-total")
+
+
+/* parrafo = document.querySelector("h6")
+parrafo.innerHTML = "Este contenido fue agregado con JavaScript" */
